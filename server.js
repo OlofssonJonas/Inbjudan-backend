8c110defbe166eeb83396bc17c24c021d3d7b907
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-02-20 13:58:46 +0100

    some shit

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const cors = require('cors')
+require('dotenv').config();
+//console.log(process.env)
 const port = 2000
 const Router = require('./src/router')
+const username = process.env.NAME
+const password = process.env.PASSWORD
+const cluster = process.env.CLUSTER
 
 
 app.use(express.json())
@@ -16,16 +21,13 @@ app.use('/api/kids', Router)
 
 
 async function init() {
-    try {
-        const options = {userNewUrlParser: true, userUnifiedTopoLogy: true}
-        await mongoose.connect('mongodb://localhost:27017/Viktoria_5')
-        console.log('connected to DB')
-    }catch (error) {
-        console.log(error)
-
-    }
-
-
-app.listen (port, () => console.log(`server is up and running on port ${port}`))
+        mongoose.set('strictQuery', true);
+        //const options = {userNewUrlParser: true, userUnifiedTopoLogy: true}
+        let uri = `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority`
+        mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopoLogy: true})
+        const connection = mongoose.connection
+        connection.once('open', () => {console.log('connected to DB')
+        app.listen (process.env.PORT || port, () => console.log(`server is up and running on port ${port}`))
+    })
 }
-init()
+init().catch ((err) => console.log(err) )
--- src/router.js
+++ src/router.js
@@ -10,25 +10,25 @@ router.get('/', async (req, res) => {
     res.send(kids)
 })
 
-// router.post('/insert', async (req, res) => {
-//     const kid = new KidModel(req.body)
-//     await kid.save()
-//     res.status(201).json(kid)
-// })
-
-router.post('/', function(req, res) {
-    var item = {
-        name: req.body.name
-    }
-    mongoose.connect('mongodb:localhost:27017/Viktoria_5', function(err, db) {
-        assert.equal(null, err);
-        db.collection('kids').insertOne(item, function(err, result) {
-            assert.equal(null, error);
-            console.log('item inserted')
-            db.close()
-        })
-    })
-    res.redirect('/')
+router.post('/', async (req, res) => {
+    const kid = new KidModel(req.body)
+    await kid.save()
+    res.status(201).json(kid)
 })
+
+// router.post('/', function(req, res) {
+//     var item = {
+//         name: req.body.name
+//     }
+//     mongoose.connect('mongodb:localhost:27017/Viktoria_5', function(err, db) {
+//         assert.equal(null, err);
+//         db.collection('kids').insertOne(item, function(err, result) {
+//             assert.equal(null, error);
+//             console.log('item inserted')
+//             db.close()
+//         })
+//     })
+//     res.redirect('/')
+// })
 
 module.exports = router
